dbfc1aefe3e8bccd3cdbf581129a70b12ac16597
Skonton <samuli.pottonen@gmail.com>
2020-03-10 13:10:46 +0200

    added empty line to list

--- list.php
+++ list.php
@@ -6,6 +6,7 @@ if($_SESSION['username'] == 'username') {
  $conn = connect_db();
  $sql = "SELECT * FROM Addresses";
  $result = mysqli_query($conn, $sql);
+
  if (mysqli_num_rows($result) > 0) {
  // output data of each row
  while($row = mysqli_fetch_assoc($result)) {
